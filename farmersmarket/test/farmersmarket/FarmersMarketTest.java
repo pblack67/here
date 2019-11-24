65e5818b8367367861d21dff9e58889eadd6d144
Paul Black <pblack67@comcast.net>
2019-11-24 14:14:09 -0600

    Replace anticipated output

--- farmersmarket/test/farmersmarket/FarmersMarketTest.java
+++ farmersmarket/test/farmersmarket/FarmersMarketTest.java
@@ -27,7 +27,7 @@ class FarmersMarketTest {
 		List<Market> markets = MarketReader.readMarkets("C:\\Users\\pblac\\Documents\\GitHub\\here\\Farmers Market\\Data\\Farmers_Market.csv");
 		MarketInput input = new MarketInput("[-81.553272, 41.098698], 5000, [Credit, WIC, Bakedgoods, Cheese, Crafts, Vegetables]");
 		List<MarketResult> results = FarmersMarket.findPercentageMarkets(markets, input);
-		assertEquals("[[1019530, 0], [1019531, 4161], [1019529, 3683]]", results.toString());
+		assertEquals("[[1019530, 0, 83.33], [1019531, 3320, 83.33], [1019533, 3352, 83.33], [1005075, 3451, 83.33], [1019545, 4513, 83.33]]", results.toString());
 	}
 
 }
